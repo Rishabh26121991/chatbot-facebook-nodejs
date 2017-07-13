f083a3694e7ce9b9a7d2dec4a62d9ee39ec2a24c
Rishabh26121991 <rishabh.srivastava@utdallas.edu>
2017-07-13 15:12:30 -0500

    ok

--- app.js
+++ app.js
@@ -62,15 +62,15 @@ app.get('/', function (req, res) {
 })
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
-	console.log("request");
-	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
-		res.status(200).send(req.query['hub.challenge']);
-	} else {
-		console.error("Failed validation. Make sure the validation tokens match.");
-		res.sendStatus(403);
-	}
-})
+// app.get('/webhook/', function (req, res) {
+// 	console.log("request");
+// 	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+// 		res.status(200).send(req.query['hub.challenge']);
+// 	} else {
+// 		console.error("Failed validation. Make sure the validation tokens match.");
+// 		res.sendStatus(403);
+// 	}
+// })
 
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
