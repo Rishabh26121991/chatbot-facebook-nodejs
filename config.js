0df298bf74c54c78d6002f434b5e5513d7ac9859
Rishabh26121991 <rishabh.srivastava@utdallas.edu>
2017-07-13 11:46:26 -0500

    commit changes for slack on fb

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: '198012771671.202296331095',
+    FB_VERIFY_TOKEN: 'TiHj3zLPmR2AkEZsdXJoJnys',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c9c68a31e6cf46c0980a6e96536d97ca',
+    FB_APP_SECRET: 'e29e76c0228bedb3bda74142214c75f8',
+    SERVER_URL: "https://nodechatbotudemy.herokuapp.com/",
 };
